cc2e7bf2e396ab8b63e08fc8287b793728d1376d
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-08-10 16:16:16 -0500

    📝 docs(layout): Improve sample grid with more controls.

--- packages/layout/sui-stories/Grid.js
+++ packages/layout/sui-stories/Grid.js
@@ -1,28 +1,77 @@
 import React, { Children } from 'react';
 
-export const Row = ({ stacked, children }) => {
-	return (
-		<div
-			className={`sui-row${
-				stacked && stacked === true
-					? ' sui-row--stacked'
-					: ' sui-row--inline'
-			}`}
-		>
-			{children}
-		</div>
-	);
+export const Row = ({
+	alignXS,
+	alignSM,
+	alignMD,
+	alignLG,
+	alignXL,
+	children,
+}) => {
+	let xs, sm, md, lg, xl;
+
+	switch (alignXS) {
+		case 'inline':
+			xs = ' sui-row--inline';
+			break;
+
+		default:
+			xs = '';
+			break;
+	}
+
+	switch (alignSM) {
+		case 'inline':
+		case 'stacked':
+			sm = ' sui-row-sm--' + alignSM;
+			break;
+
+		default:
+			sm = '';
+			break;
+	}
+
+	switch (alignMD) {
+		case 'inline':
+		case 'stacked':
+			md = ' sui-row-md--' + alignMD;
+			break;
+
+		default:
+			md = '';
+			break;
+	}
+
+	switch (alignLG) {
+		case 'inline':
+		case 'stacked':
+			lg = ' sui-row-lg--' + alignLG;
+			break;
+
+		default:
+			lg = '';
+			break;
+	}
+
+	switch (alignXL) {
+		case 'inline':
+		case 'stacked':
+			xl = ' sui-row-xl--' + alignXL;
+			break;
+
+		default:
+			xl = '';
+			break;
+	}
+
+	return <div className={`sui-row${xs}${sm}${md}${lg}${xl}`}>{children}</div>;
 };
 
-export const Col = ({ size, stacked, children }) => {
+export const Col = ({ size, children }) => {
 	return (
 		<div
 			className={`sui-col${
 				size && size !== '' ? ' sui-col--' + size : ''
-			}${
-				stacked && stacked === true
-					? ' sui-col--stacked-spacing'
-					: ' sui-col--inline-spacing'
 			}`}
 		>
 			{children}
@@ -30,7 +79,14 @@ export const Col = ({ size, stacked, children }) => {
 	);
 };
 
-export const Grid = ({ stacked, children }) => {
+export const Grid = ({
+	alignXS,
+	alignSM,
+	alignMD,
+	alignLG,
+	alignXL,
+	children,
+}) => {
 	const columns = Children.map(children, (child, index) => {
 		return (
 			<Col
@@ -43,10 +99,28 @@ export const Grid = ({ stacked, children }) => {
 		);
 	});
 
-	return <Row stacked={stacked}>{columns}</Row>;
+	return (
+		<Row
+			alignXS={alignXS}
+			alignSM={alignSM}
+			alignMD={alignMD}
+			alignLG={alignLG}
+			alignXL={alignXL}
+		>
+			{columns}
+		</Row>
+	);
 };
 
-export const GridSample = ({ size }) => {
+export const GridSample = ({
+	size,
+	border,
+	alignXS,
+	alignSM,
+	alignMD,
+	alignLG,
+	alignXL,
+}) => {
 	let numbColumns = 12;
 	let printColumns = [];
 
@@ -58,35 +132,43 @@ export const GridSample = ({ size }) => {
 		for (let i = 0; i < numbColumns; i++) {
 			printColumns.push(
 				<div key={i} size={size}>
-					<BlockSample>.sui-col--{size}</BlockSample>
+					<BlockSample border={border}>.sui-col--{size}</BlockSample>
 				</div>
 			);
 		}
 	} else {
 		printColumns.push(
 			<div size={size}>
-				<BlockSample>.sui-col--{size}</BlockSample>
+				<BlockSample border={border}>.sui-col--{size}</BlockSample>
 			</div>
 		);
 
 		printColumns.push(
 			<div size={12 - size}>
-				<BlockSample color="blue">.sui-col--{12 - size}</BlockSample>
+				<BlockSample color="blue" border={border}>
+					.sui-col--{12 - size}
+				</BlockSample>
 			</div>
 		);
 	}
 
-	return <Grid>{printColumns}</Grid>;
+	return (
+		<Grid
+			alignXS={alignXS}
+			alignSM={alignSM}
+			alignMD={alignMD}
+			alignLG={alignLG}
+			alignXL={alignXL}
+		>
+			{printColumns}
+		</Grid>
+	);
 };
 
-const BlockSample = ({ color, children }) => {
+export const BlockSample = ({ parent, color, border, children }) => {
 	const decorator = {
 		minHeight: 60,
 		overflow: 'hidden',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		padding: 5,
 		borderRadius: 4 + 'px',
 		fontSize: 10 + 'px',
 		lineHeight: 16 + 'px',
@@ -94,15 +176,32 @@ const BlockSample = ({ color, children }) => {
 		fontWeight: 'bold',
 	};
 
+	if (parent && parent === true) {
+		decorator.padding = '15px 10px';
+	} else {
+		decorator.display = 'flex';
+		decorator.alignItems = 'center';
+		decorator.justifyContent = 'center';
+		decorator.padding = 5;
+	}
+
 	switch (color) {
 		case 'blue':
 			decorator.background = '#e1eaff';
 			decorator.color = '#0056b1';
+
+			if (border && border === true) {
+				decorator.border = '1px solid #0056b1';
+			}
 			break;
 
 		default:
 			decorator.background = '#f9e1ff';
 			decorator.color = '#8d00b1';
+
+			if (border && border === true) {
+				decorator.border = '1px solid #8d00b1';
+			}
 			break;
 	}
 
--- packages/layout/sui-stories/Grid.stories.js
+++ packages/layout/sui-stories/Grid.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import docs from './Grid.mdx';
 
 import { Layout } from './Container';
-import { GridSample } from './Grid';
+import { GridSample, Row, Col, BlockSample } from './Grid';
 
 import '../scss/layout.scss';
 
@@ -16,23 +16,128 @@ export default {
 	},
 };
 
+const alignmentOptions = ['inline', 'stacked'];
+
 export const Grid = (args) => {
+	const boxStyles = {
+		padding: 20,
+		borderRadius: 3,
+		background: '#fff',
+	};
+
 	return (
-		<Layout contained={true}>
+		<Layout fullwidth={false} justify={true} spacing={true}>
+			<strong>This text needs to be aligned with the grid.</strong>{' '}
+			Vestibulum id ligula porta felis euismod semper. Morbi leo risus,
+			porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.
+			Donec ullamcorper nulla non metus auctor fringilla. Curabitur
+			blandit tempus porttitor. Donec ullamcorper nulla non metus auctor
+			fringilla. Aenean lacinia bibendum nulla sed consectetur.
+			<br />
+			&nbsp;
+			<br />
 			<GridSample {...args} />
+			<div style={boxStyles}>
+				<GridSample {...args} />
+				<Row alignMD="inline">
+					<Col size="9">
+						<BlockSample parent={true} color="blue">
+							<span
+								style={{ display: 'block', marginBottom: 10 }}
+							>
+								.sui-col--9
+							</span>
+							<GridSample border={true} {...args} />
+						</BlockSample>
+					</Col>
+					<Col size="3">
+						<BlockSample parent={true} color="blue">
+							<span
+								style={{ display: 'block', marginBottom: 10 }}
+							>
+								.sui-col--3
+							</span>
+							<BlockSample color="blue" />
+						</BlockSample>
+					</Col>
+				</Row>
+			</div>
 		</Layout>
 	);
 };
 Grid.args = {
+	alignXS: 'stacked',
+	alignSM: 'inline',
+	alignMD: 'inline',
+	alignLG: 'inline',
+	alignXL: 'inline',
 	size: 1,
 };
 Grid.argTypes = {
+	alignXS: {
+		name: 'None (Auto)',
+		control: {
+			type: 'radio',
+			options: alignmentOptions,
+		},
+		table: {
+			category: 'Row Controls',
+			subcategory: 'Alignment',
+		},
+	},
+	alignSM: {
+		name: 'Small (≥ 783px)',
+		control: {
+			type: 'radio',
+			options: alignmentOptions,
+		},
+		table: {
+			category: 'Row Controls',
+			subcategory: 'Alignment',
+		},
+	},
+	alignMD: {
+		name: 'Medium (≥ 1024px)',
+		control: {
+			type: 'radio',
+			options: alignmentOptions,
+		},
+		table: {
+			category: 'Row Controls',
+			subcategory: 'Alignment',
+		},
+	},
+	alignLG: {
+		name: 'Large (≥ 1200px)',
+		control: {
+			type: 'radio',
+			options: alignmentOptions,
+		},
+		table: {
+			category: 'Row Controls',
+			subcategory: 'Alignment',
+		},
+	},
+	alignXL: {
+		name: 'Extra Large (≥ 1600px)',
+		control: {
+			type: 'radio',
+			options: alignmentOptions,
+		},
+		table: {
+			category: 'Row Controls',
+			subcategory: 'Alignment',
+		},
+	},
 	size: {
 		name: 'Column Size',
 		control: {
 			type: 'range',
 			min: 1,
 			max: 12,
+		},
+		table: {
+			category: 'Column Controls',
 		},
 	},
 };
