a343bae4306607721d11b8901e04089de7823718
Pawan Kumar <pwnkumar201@gmail.com>
2023-03-15 12:20:50 +0530

    ✨ new(password): created new component password.

---
+++ packages/react-password/lib/react-password.js
@@ -0,0 +1,204 @@
+import React, { Fragment, useState } from 'react';
+import { Button as SuiButton } from '@wpmudev/react-button';
+import {
+	isBoolean,
+	isEmpty,
+	isObject,
+	isUndefined,
+} from '@wpmudev/react-utils';
+
+const Icon = ({ icon }) => {
+	const iconClass = 'suicons suicons--md suicons--' + icon;
+
+	return (
+		<Fragment>
+			<i className={iconClass} aria-hidden="true" />
+		</Fragment>
+	);
+};
+
+// Build "Password" component.
+const Password = ({ label, description, ...props }) => {
+	const {
+		id,
+		labelId,
+		placeholder,
+		descriptionId,
+		view,
+		errorMessage,
+		errorId,
+		size,
+		disabled,
+		value = '',
+		...inputprops
+	} = props;
+
+	const [inputValue, setInputValue] = useState(
+		!isUndefined(value) ? value : ''
+	);
+
+	const [inputType, setInputType] = useState('password');
+
+	const handleInputChange = (event) => {
+		const inputValue = event.target.value;
+		setInputValue(inputValue);
+	};
+
+	const input = {};
+
+	input.class = 'sui-input sui-input__password';
+
+	input.label = !isUndefined(label) && !isEmpty(label) ? label : '';
+	input.description =
+		!isUndefined(description) && !isEmpty(description) ? description : '';
+
+	// Input id.
+	input.id = !isUndefined(id) && !isEmpty(id) ? id : '';
+
+	// Input type.
+	input.type = inputType;
+
+	// Input placeholder.
+	input.placeholder =
+		!isUndefined(placeholder) && !isEmpty(placeholder) ? placeholder : '';
+
+	// Input aria-labelledby and label id.
+	input.labelId = !isUndefined(labelId) && !isEmpty(labelId) ? labelId : '';
+
+	// Input aria-describedby and description id.
+	input.descriptionId =
+		!isUndefined(descriptionId) && !isEmpty(descriptionId)
+			? descriptionId
+			: '';
+
+	// Input error message.
+	input.error =
+		!isUndefined(errorMessage) && !isEmpty(errorMessage)
+			? errorMessage
+			: '';
+	input.errorId = !isUndefined(errorId) && !isEmpty(errorId) ? errorId : '';
+
+	// Input view password as text or button.
+	input.view = !isUndefined(view) && !isEmpty(view) ? view : 'button';
+
+	// Renders icon element.
+	input.icon = Object.assign({
+		name: 'password' === inputType ? 'show' : 'hide',
+		size: 'md',
+		position: 'lead',
+		label: 'password' === inputType ? 'Show Password' : 'Hide Password',
+	});
+
+	const hasIcon = 'button' !== input.view ? true : false;
+
+	// Input size.
+	if (!isEmpty(size) && !isUndefined(size) && 'sm' === size) {
+		input.class += ' sui-input--sm';
+	}
+
+	// Input is disabled.
+	input.disabled =
+		!isUndefined(disabled) && isBoolean(disabled) ? disabled : false;
+
+	if (input.disabled) {
+		input.class += ' sui-input--disabled';
+	}
+
+	// Input error.
+	if (!isEmpty(input.error)) {
+		input.class += ' sui-input--error';
+	}
+
+	// if input is filled with value.
+	if (!isEmpty(inputValue)) {
+		input.class += ' sui-input--filled';
+	}
+
+	return (
+		<div className={input.class}>
+			<div className="sui-input__wrapper">
+				<input
+					value={inputValue}
+					type={input.type}
+					className="sui-input__field"
+					{...(!isEmpty(input.placeholder) && {
+						placeholder: input.placeholder,
+					})}
+					{...(!isEmpty(input.id) && { id: input.id })}
+					{...(!isEmpty(input.labelId) && {
+						'aria-labelledby': input.labelId,
+					})}
+					{...((!isEmpty(input.descriptionId) ||
+						!isEmpty(input.descriptionId)) && {
+						'aria-describedby':
+							input.descriptionId + ' ' + input.errorId,
+					})}
+					{...(!isEmpty(input.disabled) && {
+						disabled: input.disabled,
+					})}
+					onChange={handleInputChange}
+					{...inputprops}
+				/>
+				{hasIcon ? (
+					<SuiButton
+						theme="tertiary"
+						color="black"
+						icon={input.icon}
+						onClick={() => {
+							setInputType(
+								'password' === inputType ? 'text' : 'password'
+							);
+						}}
+					></SuiButton>
+				) : (
+					<SuiButton
+						theme="secondary"
+						color="black"
+						onClick={() => {
+							setInputType(
+								'password' === inputType ? 'text' : 'password'
+							);
+						}}
+					>
+						{'password' === inputType ? 'Show' : 'Hide'}
+					</SuiButton>
+				)}
+				{!isEmpty(input.label) && (
+					<label
+						className="sui-input__label"
+						{...(!isEmpty(input.id) && { htmlFor: input.id })}
+						{...(!isEmpty(input.labelId) && {
+							id: input.labelId,
+						})}
+					>
+						{input.label}
+					</label>
+				)}
+			</div>
+
+			{!isEmpty(input.error) && (
+				<span
+					{...(!isEmpty(input.errorId) && { id: input.errorId })}
+					className="sui-input__error-message"
+					role="alert"
+				>
+					{input.error}
+				</span>
+			)}
+
+			{!isEmpty(input.description) && (
+				<span
+					{...(!isEmpty(input.descriptionId) && {
+						id: input.descriptionId,
+					})}
+					className="sui-input__description"
+				>
+					{input.description}
+				</span>
+			)}
+		</div>
+	);
+};
+
+// Publish required component.
+export { Password };
---
+++ packages/react-password/stories/ReactPassword.stories.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from 'react';
+import { isUndefined, isEmpty } from '@wpmudev/react-utils';
+
+// Import required component.
+import { Password as SuiPassword } from '../lib/react-password';
+
+// Import documentation main page.
+import docs from './ReactPassword.mdx';
+
+// Configure default options.
+export default {
+	title: 'SUI/Components/Password',
+	component: SuiPassword,
+	parameters: {
+		layout: 'fullscreen',
+		docs: {
+			page: docs,
+		},
+	},
+};
+
+// Build "Password" story.
+const Password = ({
+	example,
+	label,
+	description,
+	view,
+	color,
+	size,
+	disabled,
+}) => {
+	const boxStyles = {
+		padding: 20,
+		borderRadius: 4,
+		background: color !== 'white' ? '#fff' : '#333',
+	};
+
+	const props = {};
+
+	props.color = color;
+
+	return (
+		<div className="sui-layout sui-layout--horizontal sui-layout--vertical">
+			<div className="sui-layout__content">
+				<div style={boxStyles}>
+					{'password' === example && (
+						<SuiPassword
+							id="password-default"
+							label={label}
+							value="truess"
+							labelId="label-id"
+							placeholder="Placeholder"
+							description={description}
+							descriptionId="desc-id"
+							view={view}
+							size={size}
+							disabled={disabled}
+						/>
+					)}
+
+					{'password-error' === example && (
+						<SuiPassword
+							id="password-default"
+							label={label}
+							labelId="label-id"
+							placeholder="Placeholder"
+							description={description}
+							descriptionId="desc-id"
+							errorMessage="This is an error."
+							errorId="error-id"
+							view={view}
+							size={size}
+							disabled={disabled}
+						/>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+// Set story arguments.
+Password.args = {
+	example: 'password',
+	label: 'Password',
+	description: 'Enter password.',
+	view: 'icon',
+	size: 'lg',
+	disabled: false,
+};
+
+// Set controls for story arguments.
+Password.argTypes = {
+	example: {
+		name: 'Example',
+		control: {
+			type: 'select',
+			options: {
+				'Example: Basic Password': 'password',
+				'Example: Password with error': 'password-error',
+			},
+		},
+	},
+	label: {
+		name: 'Label',
+		table: {
+			category: 'Elements',
+		},
+	},
+	description: {
+		name: 'Description',
+		table: {
+			category: 'Elements',
+		},
+	},
+	view: {
+		name: 'Hide and View',
+		control: {
+			type: 'inline-radio',
+			options: {
+				Button: 'button',
+				'Icon Button': 'icon',
+			},
+		},
+		table: {
+			category: 'Elements',
+		},
+	},
+	size: {
+		name: 'Size',
+		control: {
+			type: 'select',
+			options: {
+				Large: 'lg',
+				Small: 'sm',
+			},
+		},
+		table: {
+			category: 'Modifiers',
+		},
+	},
+	disabled: {
+		name: 'Disabled',
+		table: {
+			category: 'States',
+		},
+	},
+};
+
+// Publish required stories.
+export { Password };
