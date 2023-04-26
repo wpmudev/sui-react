3ce6d3d12d5a329aedef4530c2071020584b78f7
Leighton <2328848+iamleigh@users.noreply.github.com>
2023-04-25 20:10:13 -0500

    feat(utils): Add `isNil` and `isString` functions

--- packages/react-utils/lib/react-utils.js
+++ packages/react-utils/lib/react-utils.js
@@ -1,68 +1,95 @@
-const isNull = element => {
-	if ( null === element ) {
+const isNil = (element) => {
+	if (null == element) {
 		return true;
 	}
 
 	return false;
-}
+};
 
-const isUndefined = element => {
+const isNull = (element) => {
+	if (null === element) {
+		return true;
+	}
+
+	return false;
+};
+
+const isUndefined = (element) => {
 	return void 0 === element;
-}
+};
 
-const isObject = element => {
+const isObject = (element) => {
 	const isObject = 'object' === typeof element;
 	const isArray = Array.isArray(element);
 
-	if ( isObject && !isArray ) {
+	if (isObject && !isArray) {
 		return true;
 	}
 
 	return false;
-}
+};
 
-const isArray = element => {
+const isArray = (element) => {
 	const isObject = 'object' === typeof element;
 	const isArray = Array.isArray(element);
 
-	if ( isObject && isArray ) {
+	if (isObject && isArray) {
 		return true;
 	}
 
 	return false;
-}
+};
 
-const isBoolean = element => {
-	if ( 'boolean' === typeof element ) {
+const isBoolean = (element) => {
+	if ('boolean' === typeof element) {
 		return true;
 	}
 
 	return false;
-}
+};
 
-const isNumber = element => {
-	if ( 'number' === typeof element ) {
+const isNumber = (element) => {
+	if ('number' === typeof element) {
 		return true;
 	} else {
-		if ( !Number.isNaN( element ) ) {
+		if (!Number.isNaN(element)) {
 			return true;
 		}
 	}
 
 	return false;
-}
+};
 
-const isEmpty = element => {
-	if ( '' !== element ) {
+const isEmpty = (element) => {
+	if ('' !== element) {
 		return false;
 	}
 
 	return true;
-}
+};
 
-const isFunction = element => {
+const isFunction = (element) => {
 	return element && {}.toString.call(element) === '[object Function]';
+};
+
+const isString = (element) => {
+	if ( 'string' === typeof element ) {
+		return true;
+	}
+
+	return false;
 }
 
 // Publish required function(s).
-export { isNull, isUndefined, isObject, isArray, isBoolean, isNumber, isEmpty, isFunction }
+export {
+	isNil,
+	isNull,
+	isUndefined,
+	isObject,
+	isArray,
+	isBoolean,
+	isNumber,
+	isEmpty,
+	isFunction,
+	isString,
+};
