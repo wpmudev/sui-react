e174f15734da05a2717e3a75a47fa668458999c4
Leighton <2328848+iamleigh@users.noreply.github.com>
2023-04-30 15:56:21 -0500

    docs(text-field): Support multi-line sample

--- packages/react-field-text/stories/ReactFieldText.stories.js
+++ packages/react-field-text/stories/ReactFieldText.stories.js
@@ -1,8 +1,7 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 
 // Import modules
-import { TextField as SuiTextField, TextFieldRef } from '../lib/react-field-text';
-import { LoadingButton } from '@wpmudev/react-button';
+import { TextField as SuiTextField } from '../lib/react-field-text';
 
 // Import documentation
 import docs from './ReactFieldText.mdx';
@@ -24,9 +23,6 @@ const TextField = ({
 	example,
 	...args
 }) => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [hasError, setHasError] = useState(false);
-
 	const boxStyles = {
 		padding: 20,
 		borderRadius: 4,
@@ -37,44 +33,7 @@ const TextField = ({
 		<div className="sui-layout sui-layout--horizontal sui-layout--vertical">
 			<div className="sui-layout__content">
 				<div style={boxStyles}>
-					{'standard' === example && (
-						<SuiTextField {...args} />
-					)}
-					{'readonly' === example && (
-						<SuiTextField defaultValue="Read only field" {...args} />
-					)}
-					{'error' === example && (
-						<Fragment>
-							<TextFieldRef
-								id="fullName"
-								label="Enter a name"
-								helper='Type "Jhon Doe" in the field above to get a successful submit, or anything else to get an error message.'
-								errorMessage='You need to type "John Doe" to get a successful submit.'
-								isError={hasError} />
-							<LoadingButton
-								appearance="primary"
-								color="blue"
-								style={{margin: 0}}
-								isLoading={isLoading}
-								onClick={() => {
-									const getField = document.getElementById('fullName');
-									const getValue = getField.value;
-
-									setIsLoading(true);
-
-									setTimeout(() => {
-										if (getValue !== 'John Doe') {
-											setHasError(true);
-										} else {
-											setHasError(false);
-										}
-										setIsLoading(false);
-									}, 1000);
-								}}>
-								Submit
-							</LoadingButton>
-						</Fragment>
-					)}
+					<SuiTextField {...args} />
 				</div>
 			</div>
 		</div>
@@ -83,32 +42,21 @@ const TextField = ({
 
 // Set story arguments.
 TextField.args = {
-	example: 'standard',
 	type: 'text',
 	id: 'myCustomId',
-	label: 'Full Name',
-	helper: 'Cras mattis consectetur purus sit amet fermentum.',
-	errorMessage: 'Maecenas faucibus mollis interdum.',
+	label: 'Label text',
+	helper: 'Supporting text goes here',
+	errorMessage: 'Something went wrong',
+	isMultiLine: false,
 	isLabelHidden: false,
 	isSmall: false,
 	isError: false,
-	isReadOnly: true,
+	isReadOnly: false,
 	isDisabled: false,
 };
 
 // Set controls for story arguments.
 TextField.argTypes = {
-	example: {
-		name: 'Example',
-		control: {
-			type: 'select',
-			options: {
-				'Example: Standard': 'standard',
-				'Example: Read Only': 'readonly',
-				'Example: Error interactive': 'error'
-			},
-		},
-	},
 	type: {
 		name: 'Type',
 		control: {
@@ -116,77 +64,56 @@ TextField.argTypes = {
 			options: ['text', 'email', 'password', 'number', 'search', 'tel', 'url']
 		},
 		if: {
-			arg: 'example',
-			neq: 'error'
+			arg: 'isMultiLine',
+			eq: false
 		}
 	},
 	id: {
 		name: 'ID',
 		control: 'text',
-		if: {
-			arg: 'example',
-			neq: 'error'
-		}
 	},
 	label: {
 		name: 'Label',
-		if: {
-			arg: 'example',
-			neq: 'error'
-		}
+		control: 'text',
 	},
 	helper: {
 		name: 'Helper',
-		if: {
-			arg: 'example',
-			neq: 'error'
-		}
+		control: 'text',
 	},
 	errorMessage: {
 		name: 'Error Message',
-		if: {
-			arg: 'example',
-			neq: 'error'
-		}
+		control: 'text',
+	},
+	isMultiLine: {
+		name: 'Multi Line',
+		control: 'boolean',
 	},
 	isSmall: {
 		name: 'Small',
 		control: 'boolean',
-		if: {
-			arg: 'example',
-			neq: 'error'
-		}
 	},
 	isLabelHidden: {
 		name: 'SR Label',
 		control: 'boolean',
-		if: {
-			arg: 'example',
-			neq: 'error'
-		}
 	},
 	isError: {
 		name: 'Error',
 		control: 'boolean',
 		if: {
-			arg: 'example',
-			eq: 'standard'
+			arg: 'isReadOnly',
+			eq: false
 		}
 	},
 	isReadOnly: {
 		name: 'Read Only',
 		control: 'boolean',
-		if: {
-			arg: 'example',
-			eq: 'readonly'
-		}
 	},
 	isDisabled: {
 		name: 'Disabled',
 		control: 'boolean',
 		if: {
-			arg: 'example',
-			eq: 'standard'
+			arg: 'isReadOnly',
+			eq: false
 		}
 	},
 };
