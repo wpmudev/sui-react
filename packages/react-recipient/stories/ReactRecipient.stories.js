beef45cdd089d8c900d12969547917cde5fd06dd
Leighton <2328848+iamleigh@users.noreply.github.com>
2023-05-11 20:54:14 -0500

    docs(recipient): Relocate story

--- packages/react-recipient/stories/ReactRecipient.stories.js
+++ packages/react-recipient/stories/ReactRecipient.stories.js
@@ -11,7 +11,7 @@ import image from './images/unreal-person.jpg';
 
 // Configure default options.
 export default {
-	title: 'SUI/Components/Simple Elements/Recipient',
+	title: 'SUI/Components/Compound Elements/Recipient',
 	component: Recipient,
 	parameters: {
 		layout: 'fullscreen',
