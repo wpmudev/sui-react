50beb8bc2188cef6cef0eb4e745aae8934bd9e06
Govind Kumar <gkprmr@gmail.com>
2023-07-11 19:39:40 +0530

    doc(react-field-text): update doc

--- packages/react-field-text/stories/ReactFieldText.stories.js
+++ packages/react-field-text/stories/ReactFieldText.stories.js
@@ -1,33 +1,30 @@
-import React from 'react';
+import React from "react"
 
 // Import modules
-import { TextField as SuiTextField } from '../lib/react-field-text';
+import { TextField as SuiTextField } from "../src"
 
 // Import documentation
-import docs from './ReactFieldText.mdx';
+import docs from "./ReactFieldText.mdx"
 
 // Configure default options.
 export default {
-	title: 'SUI/Components/Form Elements/Text Field',
+	title: "SUI/Components/Form Elements/Text Field",
 	component: SuiTextField,
 	parameters: {
-		layout: 'fullscreen',
+		layout: "fullscreen",
 		docs: {
 			page: docs,
 		},
 	},
-};
+}
 
 // Build "Text Field" story.
-const TextField = ({
-	example,
-	...args
-}) => {
+const TextField = ({ example, ...args }) => {
 	const boxStyles = {
 		padding: 20,
 		borderRadius: 4,
-		background: '#fff',
-	};
+		background: "#fff",
+	}
 
 	return (
 		<div className="sui-layout sui-layout--horizontal sui-layout--vertical">
@@ -37,86 +34,86 @@ const TextField = ({
 				</div>
 			</div>
 		</div>
-	);
-};
+	)
+}
 
 // Set story arguments.
 TextField.args = {
-	type: 'text',
-	id: 'myCustomId',
-	label: 'Label text',
-	helper: 'Supporting text goes here',
-	errorMessage: 'Something went wrong',
+	type: "text",
+	id: "myCustomId",
+	label: "Label text",
+	helper: "Supporting text goes here",
+	errorMessage: "Something went wrong",
 	isMultiLine: false,
 	isLabelHidden: false,
 	isSmall: false,
 	isError: false,
 	isReadOnly: false,
 	isDisabled: false,
-};
+}
 
 // Set controls for story arguments.
 TextField.argTypes = {
 	type: {
-		name: 'Type',
+		name: "Type",
 		control: {
-			type: 'select',
-			options: ['text', 'email', 'password', 'number', 'search', 'tel', 'url']
+			type: "select",
+			options: ["text", "email", "password", "number", "search", "tel", "url"],
 		},
 		if: {
-			arg: 'isMultiLine',
-			eq: false
-		}
+			arg: "isMultiLine",
+			eq: false,
+		},
 	},
 	id: {
-		name: 'ID',
-		control: 'text',
+		name: "ID",
+		control: "text",
 	},
 	label: {
-		name: 'Label',
-		control: 'text',
+		name: "Label",
+		control: "text",
 	},
 	helper: {
-		name: 'Helper',
-		control: 'text',
+		name: "Helper",
+		control: "text",
 	},
 	errorMessage: {
-		name: 'Error Message',
-		control: 'text',
+		name: "Error Message",
+		control: "text",
 	},
 	isMultiLine: {
-		name: 'Multi Line',
-		control: 'boolean',
+		name: "Multi Line",
+		control: "boolean",
 	},
 	isSmall: {
-		name: 'Small',
-		control: 'boolean',
+		name: "Small",
+		control: "boolean",
 	},
 	isLabelHidden: {
-		name: 'SR Label',
-		control: 'boolean',
+		name: "SR Label",
+		control: "boolean",
 	},
 	isError: {
-		name: 'Error',
-		control: 'boolean',
+		name: "Error",
+		control: "boolean",
 		if: {
-			arg: 'isReadOnly',
-			eq: false
-		}
+			arg: "isReadOnly",
+			eq: false,
+		},
 	},
 	isReadOnly: {
-		name: 'Read Only',
-		control: 'boolean',
+		name: "Read Only",
+		control: "boolean",
 	},
 	isDisabled: {
-		name: 'Disabled',
-		control: 'boolean',
+		name: "Disabled",
+		control: "boolean",
 		if: {
-			arg: 'isReadOnly',
-			eq: false
-		}
+			arg: "isReadOnly",
+			eq: false,
+		},
 	},
-};
+}
 
 // Publish required stories.
-export { TextField };
+export { TextField }
