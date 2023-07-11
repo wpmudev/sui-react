3fa7fdafba94b517d337fd960ef6a724321e936d
Govind Kumar <gkprmr@gmail.com>
2023-07-11 16:41:11 +0530

    doc(react-hooks): code improvements

--- packages/react-hooks/lib/index.ts
+++ packages/react-hooks/lib/index.ts
@@ -1,43 +1,13 @@
-import classnames from 'classnames';
-import React, { MouseEventHandler, useCallback, useState } from 'react';
-import { isFunction } from '@wpmudev/react-utils';
-
-export interface GlobalStylesProps {
-	/**
-	 * Base classname
-	 * @type string
-	 */
-	base: string;
-	/**
-	 * SUI Theme appearance
-	 * @type boolean | string
-	 */
-	appearance: boolean | string;
-	/**
-	 * SUI Theme color
-	 * @type boolean | string
-	 */
-	color: boolean | string;
-}
-
-const useGlobalStyles = ({
-	base = '',
-	appearance,
-	color,
-}: GlobalStylesProps) => {
-	// classnames
-	const classNames = classnames(base, {
-		[`${base}-${appearance}-${color}`]: !!appearance && !!color,
-	});
-
-	return { classNames };
-};
+import { useCallback, useState } from "react"
 
 export interface InteractionTypes {
-	onMouseEnter?: (e: MouseEvent) => void;
-	onMouseLeave?: (e: MouseEvent) => void;
-	onFocus?: (e: FocusEvent) => void;
-	onBlur?: (e: FocusEvent) => void;
+	onMouseEnter?: (e: MouseEvent) => void
+	onMouseLeave?: (e: MouseEvent) => void
+	onMouseDownCapture?: (e: MouseEvent) => void
+	onMouseUpCapture?: (e: MouseEvent) => void
+	onFocus?: (e: FocusEvent) => void
+	onBlur?: (e: FocusEvent) => void
+	onBlurCapture?: (e: FocusEvent) => void
 }
 
 /**
@@ -45,80 +15,119 @@ export interface InteractionTypes {
  *
  * @param {any} onMouseEnter
  * @param {any} onMouseLeave
+ * @param {any} onMouseDownCapture
+ * @param {any} onMouseUpCapture
  * @param {any} onFocus
  * @param {any} onBlur
+ * @param onBlurCapture
  *
  * @return {{isHovered: boolean, methods: {onMouseEnter: (e: Event) => void, onMouseLeave: (e: Event) => void}, isFocused: boolean}}
  */
 const useInteraction = ({
-	onMouseEnter,
-	onMouseLeave,
-	onFocus,
-	onBlur,
+	onMouseEnter = () => {},
+	onMouseLeave = () => {},
+	onMouseDownCapture = () => {},
+	onMouseUpCapture = () => {},
+	onFocus = () => {},
+	onBlur = () => {},
+	onBlurCapture = () => {},
 }: InteractionTypes) => {
-	const [isHovered, setIsHovered] = useState<boolean>(false);
-	const [isFocused, setIsFocused] = useState<boolean>(false);
+	const [isHovered, setIsHovered] = useState<boolean>(false)
+	const [isFocused, setIsFocused] = useState<boolean>(false)
+
+	/**
+	 * Toggle hover state
+	 */
+	const toggleHover = useCallback((state, callback, event) => {
+		setIsHovered(state)
+
+		// execute callback
+		if (callback) {
+			callback(event)
+		}
+	}, [])
+
+	/**
+	 * Toggle focus state
+	 */
+	const toggleFocus = useCallback((state, callback, event) => {
+		setIsFocused(state)
+
+		// execute callback
+		if (callback) {
+			callback(event)
+		}
+	}, [])
 
 	/**
 	 * When mouse enters
+	 *
 	 * @type {(e: Event) => void}
 	 */
 	const onMouseEnterCallback = useCallback(
-		(e: MouseEvent) => {
-			setIsHovered(true);
-
-			// execute if method exists
-			if (onMouseEnter) {
-				onMouseEnter(e);
-			}
-		},
-		[isHovered]
-	);
+		(e: MouseEvent) => toggleHover(true, onMouseEnter, e),
+		[onMouseEnter, toggleHover],
+	)
 
 	/**
 	 * When mouse leaves
+	 *
 	 * @type {(e: Event) => void}
 	 */
 	const onMouseLeaveCallback = useCallback(
-		(e: MouseEvent) => {
-			setIsHovered(false);
-			// execute if method exists
-			if (onMouseLeave) {
-				onMouseLeave(e);
-			}
-		},
-		[isHovered]
-	);
+		(e: MouseEvent) => toggleHover(false, onMouseLeave, e),
+		[onMouseLeave, toggleHover],
+	)
 
 	/**
-	 * When mouse leaves
+	 * When mouse down capture
+	 *
 	 * @type {(e: Event) => void}
 	 */
-	const onFocusCallback = useCallback(
-		(e: FocusEvent) => {
-			setIsFocused(true);
-			// execute if method exists
-			if (onFocus) {
-				onFocus(e);
-			}
-		},
-		[isFocused]
-	);
+	const onMouseDownCaptureCallback = useCallback(
+		(e: MouseEvent) => toggleFocus(false, onMouseDownCapture, e),
+		[onMouseDownCapture, toggleFocus],
+	)
 
 	/**
-	 * When mouse leaves
+	 * When mouse up capture
+	 *
+	 * @type {(e: Event) => void}
+	 */
+	const onMouseUpCaptureCallback = useCallback(
+		(e: MouseEvent) => toggleFocus(true, onMouseUpCapture, e),
+		[onMouseUpCapture, toggleFocus],
+	)
+
+	/**
+	 * When mouse up capture
+	 *
+	 * @type {(e: Event) => void}
+	 */
+	const onBlurCaptureCallback = useCallback(
+		(e: MouseEvent) => toggleHover(false, onBlurCapture, e),
+		[onBlurCapture, toggleHover],
+	)
+
+	/**
+	 * When mouse up capture
+	 *
 	 * @type {(e: Event) => void}
 	 */
 	const onBlurCallback = useCallback(
-		(e: FocusEvent) => {
-			setIsFocused(false);
-			// execute if method exists
-			if (onBlur) {
-				onBlur(e);
-			}
-		},
-		[isHovered]
-	);
+		(e: MouseEvent) => toggleFocus(false, onBlur, e),
+		[onBlur, toggleFocus],
+	)
+
+	/**
+	 * When mouse up capture
+	 *
+	 * @type {(e: Event) => void}
+	 */
+	const onFocusCallback = useCallback(
+		(e: MouseEvent) => toggleFocus(true, onFocus, e),
+		[onFocus, toggleFocus],
+	)
 
 	return [
 		isHovered,
@@ -126,10 +135,13 @@ const useInteraction = ({
 		{
 			onMouseEnter: onMouseEnterCallback,
 			onMouseLeave: onMouseLeaveCallback,
+			onMouseDownCapture: onMouseDownCaptureCallback,
+			onMouseUpCapture: onMouseUpCaptureCallback,
 			onFocus: onFocusCallback,
 			onBlur: onBlurCallback,
+			onBlurCapture: onBlurCaptureCallback,
 		},
-	] as const;
-};
+	] as const
+}
 
-export { useGlobalStyles, useInteraction };
+export { useInteraction }
