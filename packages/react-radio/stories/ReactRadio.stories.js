9f7bbeff271b9d035dc12c4bebb6e1e7916d6b36
Govind Kumar <gkprmr@gmail.com>
2023-07-11 17:55:47 +0530

    refactor(react-radio): code improvement and transform to ts

--- packages/react-radio/stories/ReactRadio.stories.js
+++ packages/react-radio/stories/ReactRadio.stories.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react"
 
 // Import required modules
-import { Radio as SuiRadio } from '../lib/react-radio';
+import { Radio as SuiRadio } from "../src"
 
 // Import documentation
-import docs from './ReactRadio.mdx';
+import docs from "./ReactRadio.mdx"
 
 // Default settings
 export default {
-	title: 'SUI/Components/Form Elements/Radio',
+	title: "SUI/Components/Form Elements/Radio",
 	component: SuiRadio,
 	parameters: {
-		layout: 'fullscreen',
+		layout: "fullscreen",
 		docs: {
 			page: docs,
 		},
 	},
-};
+}
 
 // Build story
-export const Radio = ({...args}) => {
+export const Radio = ({ ...args }) => {
 	const boxStyles = {
 		padding: 20,
 		borderRadius: 4,
-		background: '#fff',
-	};
+		background: "#fff",
+	}
 
 	return (
 		<div className="sui-layout sui-layout--horizontal sui-layout--vertical">
@@ -34,21 +34,21 @@ export const Radio = ({...args}) => {
 				</div>
 			</div>
 		</div>
-	);
+	)
 }
 
 // Story props defaults
 Radio.args = {
-	label: 'Radio label',
+	label: "Radio label",
 	isSmall: false,
 	isDisabled: false,
-};
+}
 
 // Story props settings
 Radio.argTypes = {
 	isChecked: {
 		table: {
-			disable: true
-		}
-	}
-};
+			disable: true,
+		},
+	},
+}
