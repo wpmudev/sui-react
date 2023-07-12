766d24e5b2cd46824e1aff90e6511565d9da8909
Govind Kumar <gkprmr@gmail.com>
2023-07-12 16:45:59 +0530

    fix(react-checkbox): add codes doc

--- packages/react-checkbox/src/index.tsx
+++ packages/react-checkbox/src/index.tsx
@@ -10,12 +10,30 @@ import { generateCN } from "@wpmudev/react-utils"
 
 import { Tick } from "./elements/tick"
 
+/**
+ * Represents the properties for a checkbox component.
+ */
 interface CheckboxPropsTypes
 	extends Omit<HTMLProps<HTMLInputElement>, "defaultValue"> {
+	/**
+	 * The label for the checkbox.
+	 */
 	label?: string
+	/**
+	 * Indicates whether the label should be hidden or not.
+	 */
 	isLabelHidden?: boolean
+	/**
+	 * Indicates whether the checkbox should be displayed as small or not.
+	 */
 	isSmall?: boolean
+	/**
+	 * Indicates whether the checkbox is disabled or not.
+	 */
 	isDisabled?: boolean
+	/**
+	 * The default value of the checkbox.
+	 */
 	defaultValue?: boolean
 }
 
@@ -25,7 +43,10 @@ interface CheckboxPropsTypes
  * React component used in forms when a user needs to select
  * multiple values from several options.
  */
-const Checkbox = forwardRef<HTMLInputElement, CheckboxPropsTypes>(
+const Checkbox: React.FC<CheckboxPropsTypes> = forwardRef<
+	HTMLInputElement,
+	CheckboxPropsTypes
+>(
 	(
 		{
 			label,
