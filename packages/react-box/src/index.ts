e03b00403f1eeb7813b706b8efc8dc1d8d5e84bd
Govind Kumar <gkprmr@gmail.com>
2023-07-12 16:20:35 +0530

    refactor(react-box): code improvements + transform to TS

---
+++ packages/react-box/src/index.ts
@@ -0,0 +1,6 @@
+// Get required component(s)
+import { Box, BoxProps } from "./elements/box"
+import { BoxGroup, BoxGroupProps } from "./elements/box-group"
+
+// Publish component(s)
+export { Box, BoxProps, BoxGroup, BoxGroupProps }
