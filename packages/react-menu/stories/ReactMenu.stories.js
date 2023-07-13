a7f0d2aa7d09442cccbba39d15280c2740e0ce1a
Govind Kumar <gkprmr@gmail.com>
2023-07-13 08:31:21 +0530

    doc(react-menu): update stories

--- packages/react-menu/stories/ReactMenu.stories.js
+++ packages/react-menu/stories/ReactMenu.stories.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react"
 
 // Import required component
-import { Menu as SuiMenu } from '../lib/react-menu';
-import { Box, BoxGroup } from '@wpmudev/react-box';
+import { Menu as SuiMenu } from "../src"
+import { Box, BoxGroup } from "@wpmudev/react-box"
 
 // Import documentation main page
-import docs from './ReactMenu.mdx';
+import docs from "./ReactMenu.mdx"
 
 // Configure default options
 export default {
-	title: 'SUI/Components/Simple Elements/Menu',
+	title: "SUI/Components/Simple Elements/Menu",
 	component: SuiMenu,
 	parameters: {
-		layout: 'fullscreen',
+		layout: "fullscreen",
 		docs: {
 			page: docs,
 		},
 	},
-};
+}
 
 // Build menu story
 const Menu = ({ example }) => {
@@ -27,40 +27,64 @@ const Menu = ({ example }) => {
 				<Box>
 					<BoxGroup>
 						<SuiMenu>
-							<span label="Free Plugins" {...('link' === example && {href: '#'})} />
-							<span label="Membership" {...('link' === example && {href: '#'})} />
-							<span label="Roadmap" {...('link' === example && {href: '#'})} />
-							<span label="Support" {...('link' === example && {href: '#'})} />
-							<span label="Community" {...('link' === example && {href: '#'})} />
-							<span label="The Hub" {...('link' === example && {href: '#'})} />
-							<span label="Terms of Service" {...('link' === example && {href: '#'})} />
-							<span label="Privacy Policy" {...('link' === example && {href: '#'})} />
+							<span
+								label="Free Plugins"
+								{...("link" === example && { href: "#" })}
+							/>
+							<span
+								label="Membership"
+								{...("link" === example && { href: "#" })}
+							/>
+							<span
+								label="Roadmap"
+								{...("link" === example && { href: "#" })}
+							/>
+							<span
+								label="Support"
+								{...("link" === example && { href: "#" })}
+							/>
+							<span
+								label="Community"
+								{...("link" === example && { href: "#" })}
+							/>
+							<span
+								label="The Hub"
+								{...("link" === example && { href: "#" })}
+							/>
+							<span
+								label="Terms of Service"
+								{...("link" === example && { href: "#" })}
+							/>
+							<span
+								label="Privacy Policy"
+								{...("link" === example && { href: "#" })}
+							/>
 						</SuiMenu>
 					</BoxGroup>
 				</Box>
 			</div>
 		</div>
-	);
-};
+	)
+}
 
 // Set story arguments
 Menu.args = {
-	example: 'default',
-};
+	example: "default",
+}
 
 // Set controls for story arguments
 Menu.argTypes = {
 	example: {
-		name: 'Example',
+		name: "Example",
 		control: {
-			type: 'select',
+			type: "select",
 			options: {
-				'Example: Default': 'default',
-				'Example: Link': 'link',
+				"Example: Default": "default",
+				"Example: Link": "link",
 			},
 		},
-	}
-};
+	},
+}
 
 // Publish required stories
-export { Menu };
+export { Menu }
