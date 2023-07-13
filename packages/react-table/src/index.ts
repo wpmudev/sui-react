c89a67a19aad93df1fd624278bf85327adb9f533
Govind Kumar <gkprmr@gmail.com>
2023-07-13 08:15:19 +0530

    refactor(react-table): code improvement + transform to TS

---
+++ packages/react-table/src/index.ts
@@ -0,0 +1,12 @@
+// export table types
+export type { TableProps, TableSectionProps } from "./table"
+export type { TableCellProps } from "./table-cell"
+export type { TableRowProps } from "./table-row"
+
+// export table components
+export { Table } from "./table"
+export { TableBody } from "./table-body"
+export { TableRow } from "./table-row"
+export { TableHead } from "./table-head"
+export { TableFooter } from "./table-footer"
+export { TableCell } from "./table-cell"
