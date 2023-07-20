400593f0c8c5d90aaa690bdd2aba97cd6b5b9969
Govind Kumar <gkprmr@gmail.com>
2023-07-20 10:30:20 +0530

    feat(hooks): introduce new hook to detect outer clicks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,6 @@
 // useInteraction hook
 export { useInteraction } from "./use-intraction"
 export type { InteractionTypes } from "./use-intraction"
+
+// useOuterClick hook
+export { useOuterClick } from "./use-outer-click"
