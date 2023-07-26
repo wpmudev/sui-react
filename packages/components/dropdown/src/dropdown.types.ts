8f5a0c40db16bf2a524418bb124cfa33c799d789
Govind Kumar <gkprmr@gmail.com>
2023-07-26 11:34:54 +0530

    fix(dropdown): type export

--- packages/components/dropdown/src/dropdown.types.ts
+++ packages/components/dropdown/src/dropdown.types.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { HTMLProps } from "react"
 
 /**
  * Represents the properties for a dropdown component.
@@ -34,4 +34,54 @@ interface DropdownProps {
 	children?: React.ReactNode
 }
 
-export { DropdownProps}
+/**
+ * Represents the properties for a dropdown option.
+ */
+type DropdownOptionProps = {
+	/**
+	 * The icon for the dropdown option.
+	 */
+	icon?: string
+	/**
+	 * The URL for the dropdown option.
+	 */
+	href?: string
+	/**
+	 * The callback function to be called when the dropdown option is clicked.
+	 */
+	onClick?: () => void
+	/**
+	 * The content of the dropdown option.
+	 */
+	children?: React.ReactNode
+} & Omit<
+	HTMLProps<HTMLAnchorElement | HTMLButtonElement | HTMLDivElement>,
+	"onClick" | "onSelect"
+>
+
+/**
+ * Represents the properties for a dropdown option item.
+ */
+interface DropdownOptionItemProps extends DropdownOptionProps {
+	/**
+	 * The unique identifier of the dropdown option item.
+	 */
+	id: string
+	/**
+	 * Indicates whether the dropdown option item is active or not.
+	 */
+	isActive: boolean
+	/**
+	 * The index of the dropdown option item.
+	 */
+	index: number
+	/**
+	 * The callback function to be called when the dropdown option item is selected.
+	 *
+	 * @param option - The selected dropdown option.
+	 * @param index  - The index of the selected dropdown option.
+	 */
+	onSelect: (option: DropdownOptionProps, index: number) => void
+}
+
+export { DropdownProps, DropdownOptionProps, DropdownOptionItemProps }
