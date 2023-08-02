093b669d47838e40246186ff0e78968f34122c84
Govind Kumar <gkprmr@gmail.com>
2023-08-02 07:54:33 +0530

    fix(notification): add fluid size variant

--- packages/components/notification/src/notification.types.ts
+++ packages/components/notification/src/notification.types.ts
@@ -1,7 +1,7 @@
 /*
  * Props for the Notification component.
  */
-import React, { HTMLProps, ReactNode } from "react"
+import { ReactNode } from "react"
 
 // interface definition for the NotificationProps
 interface NotificationProps {
@@ -14,56 +14,8 @@ interface NotificationProps {
 	isInline?: boolean // flag to indicate if the notification is displayed inline
 	isDismissible?: boolean // make notification dismissible
 	timeout?: number // make notification invisible
-	size?: "sm" | "lg" // size variation for the notification (small or large)
+	size?: "sm" | "lg" | "fluid" // size variation for the notification (small, large, or fluid)
 	variation?: "info" | "success" | "warning" | "error" // variation type for the notification
-}
-
-interface NotificationProps1
-	extends Pick<HTMLProps<HTMLDivElement>, "onClick"> {
-	/**
-	 * The unique identifier for the notification.
-	 */
-	id?: string
-	/**
-	 * The title of the notification.
-	 */
-	title?: string
-	/**
-	 * The action associated with the notification.
-	 */
-	action?: string
-	/**
-	 * The content of the notification.
-	 */
-	children?: React.ReactNode
-	/**
-	 * The state of the notification (e.g., success, error, warning).
-	 */
-	state?: string
-	/**
-	 * The time when the notification appears.
-	 */
-	timeIn: number
-	/**
-	 * The time when the notification disappears.
-	 */
-	timeOut: number
-	/**
-	 * Flag indicating if the notification should be small.
-	 */
-	isSmall?: boolean
-	/**
-	 * Flag indicating if the notification should be inline.
-	 */
-	isInline?: boolean
-	/**
-	 * Flag indicating if the notification is visible.
-	 */
-	isVisible: boolean
-	/**
-	 * Flag indicating if the notification should have a close button.
-	 */
-	isCloseButton?: boolean
 }
 
 export { NotificationProps }
