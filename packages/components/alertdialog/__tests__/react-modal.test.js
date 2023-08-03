1630bf00df8829e6e38177c925c665413dd14bfa
Govind Kumar <gkprmr@gmail.com>
2023-08-03 14:21:27 +0530

    chore: delete alert-dialog

--- packages/components/alertdialog/__tests__/react-modal.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const reactModal = require('../src');
-
-describe('@wpmudev/react-alertdialog', () => {
-    it('needs tests');
-});
--- packages/components/alertdialog/src/index.ts
+++
@@ -1,4 +0,0 @@
-// export components
-export { AlertDialog } from "./alertdialog"
-// export types
-export type { AlertDialogProps } from "./alertdialog.types"
