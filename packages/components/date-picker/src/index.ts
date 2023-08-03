fd4f03fcce495e888d2ccbd5223d998cef932f21
Govind Kumar <gkprmr@gmail.com>
2023-08-03 14:08:00 +0530

    fix(date-picker): export date picker types

--- packages/components/date-picker/src/index.ts
+++ packages/components/date-picker/src/index.ts
@@ -1,2 +1,8 @@
 export { DatePicker } from "./date-picker"
-export type { DatePickerProps } from "./date-picker.types"
+export type {
+	DatePickerProps,
+	DatePickerDateRange,
+	DatePickerContextProps,
+	DatePickerNavs,
+	DatePickerTypes,
+} from "./date-picker.types"
