3ace6114085af79be03409ccd6cc15462d8f0e4b
douara <abderrahmandouara@gmail.com>
2023-08-11 18:35:56 +0800

    new(SegmentedControl): Documentation, Code Page

--- packages/components/segmented-control/stories/SegmentedControl.stories.tsx
+++ packages/components/segmented-control/stories/SegmentedControl.stories.tsx
@@ -26,7 +26,7 @@ const SegmentedControl = ({ example, children, ...args }) => {
 			<div className="sui-layout__content">
 				<Box>
 					<BoxGroup isInline={false}>
-						<SControl {...args}>
+						<SControl onChange={(newValue) => alert(newValue)} {...args}>
 							<SegmentedControlButton value="icon-only" icon="Filter" />
 							<SegmentedControlButton value="1">UPI</SegmentedControlButton>
 							<SegmentedControlButton value="2">Visa</SegmentedControlButton>
