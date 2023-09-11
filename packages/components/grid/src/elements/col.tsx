22c2754e307603ef567537bb9f9e880c9b2980dc
Govind Kumar <gkprmr@gmail.com>
2023-09-11 07:14:23 +0530

    fix(cols): pass props

--- packages/components/grid/src/elements/col.tsx
+++ packages/components/grid/src/elements/col.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { generateCN } from "@wpmudev/sui-utils"
 import { ColProps } from "../grid.types"
 
-const Col: React.FC<ColProps> = ({ size, children, className }) => {
+const Col: React.FC<ColProps> = ({ size, children, className, ...props }) => {
 	const classNames = generateCN(
 		"sui-col",
 		{
@@ -13,7 +13,11 @@ const Col: React.FC<ColProps> = ({ size, children, className }) => {
 		className,
 	)
 
-	return <div className={classNames}>{children}</div>
+	return (
+		<div className={classNames} {...props}>
+			{children}
+		</div>
+	)
 }
 
 export { Col }
