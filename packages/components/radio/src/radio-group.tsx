3346671cc510f1cb5f02c5b9217b92c86aa26056
Pawan Kumar <pwnkumar201@gmail.com>
2023-09-21 17:43:34 +0530

    ✨ new(radio): radio name prop and onchange fix.

--- packages/components/radio/src/radio-group.tsx
+++ packages/components/radio/src/radio-group.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react"
 import { generateCN } from "@wpmudev/sui-utils"
 
 interface RadioGroupProps {
+	name: string
 	isInline: boolean
 	children: ReactNode
 }
@@ -9,16 +10,18 @@ interface RadioGroupProps {
 const RadioGroup = ({
 	isInline = true,
 	children,
+	name = "",
 	...props
 }: RadioGroupProps) => {
 	return (
 		<div
 			className={generateCN("sui-radio-group", { inline: isInline })}
 			role="radiogroup"
+			{...props}
 		>
 			{React.Children.map(children, (child) =>
 				React.cloneElement(child as React.ReactElement<any>, {
-					...props,
+					name,
 				}),
 			)}
 		</div>
