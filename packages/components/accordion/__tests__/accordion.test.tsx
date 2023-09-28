52543c4b80ae71fe209129ba9a223552ca28029a
Govind Kumar <gkprmr@gmail.com>
2023-09-28 12:41:44 +0530

    fix(accordion): code improve

---
+++ packages/components/accordion/__tests__/accordion.test.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import "@testing-library/jest-dom"
+import { render, screen } from "@testing-library/react"
+
+const Accordion = require("../src")
+
+describe("@wpmudev/sui-accordion", () => {
+	it("needs tests")
+})
