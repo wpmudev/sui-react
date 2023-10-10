f91f3766985a7d04bc3620d78c7f67e8fa4449ad
Govind <gkprmr@gmail.com>
2023-10-10 15:27:59 +0530

    Update alert-banner.tsx

--- packages/components/alert-banner/src/alert-banner.tsx
+++ packages/components/alert-banner/src/alert-banner.tsx
@@ -24,7 +24,7 @@ const AlertBanner: React.FC< AlertBannerProps  > =
 	 */
 	const onClose = useCallback(() => {
 		setIsVisible(false)
-		if (!!onDismiss) {
+		if (!!onDismiss) { 
 			onDismiss() 
 		}
 	}, [onDismiss])
