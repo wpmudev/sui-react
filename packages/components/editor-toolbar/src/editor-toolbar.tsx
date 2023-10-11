1800b272c5b5fbbdf36e9b60c8b0ca652371fb6b
Pawan Kumar <pwnkumar201@gmail.com>
2023-10-11 15:46:35 +0530

    ✨ new(design): design check of editor toolbar.

--- packages/components/editor-toolbar/src/editor-toolbar.tsx
+++ packages/components/editor-toolbar/src/editor-toolbar.tsx
@@ -48,14 +48,16 @@ const EditorToolbar: FC<EditorToolbarProps> = ({
 				<div className="sui-editor-toolbar__content--left">
 					<div className="sui-editor-toolbar__main">
 						{allowBack && (
-							<Button
-								appearance="tertiary"
-								icon="chevron-left"
-								color="black"
-								isSmall={true}
-								iconOnly={true}
-								onClick={onBackClick}
-							/>
+							<div>
+								<Button
+									appearance="tertiary"
+									icon="chevron-left"
+									color="black"
+									isSmall={true}
+									iconOnly={true}
+									onClick={onBackClick}
+								/>
+							</div>
 						)}
 						{title && (
 							<h3 className="sui-heading--h4 sui-editor-toolbar__main-title">
