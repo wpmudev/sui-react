89ffc07e4815087ffcebb4a27425aa88e0c8434f
Govind Kumar <gkprmr@gmail.com>
2023-10-15 07:09:19 +0530

    build: improve gulp script and npm tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,16 @@
  *
  * @since 1.0.0
  */
-const path = require('path');
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const header = require('gulp-header');
-const prettier = require('gulp-prettier');
+const path = require("path")
+const gulp = require("gulp")
+const rename = require("gulp-rename")
+const sass = require("gulp-sass")(require("sass"))
+const cleanCSS = require("gulp-clean-css")
+const autoprefixer = require("gulp-autoprefixer")
+const header = require("gulp-header")
+const prettier = require("gulp-prettier")
+const ts = require("gulp-typescript")
+const { exec } = require("child_process")
 
 /**
  * Development Paths & Files
@@ -19,17 +21,20 @@ const prettier = require('gulp-prettier');
  * @since 1.0.0
  */
 
-const currentWorkingPath = process.cwd();
-const { input, output, name } = require(path.join(currentWorkingPath, 'package.json'));
+const currentWorkingPath = process.cwd()
+const { input, output, name } = require(path.join(
+	currentWorkingPath,
+	"package.json",
+))
 
-const packageName = name;
-const fileName = packageName.replace('@wpmudev/', '');
+const packageName = name
+const fileName = packageName.replace("@wpmudev/", "")
 
-const inputSource = path.join(currentWorkingPath, input);
-const inputStyles = path.join(inputSource, 'scss/' + fileName + '.scss');
+const inputSource = path.join(currentWorkingPath, input)
+const inputStyles = path.join(inputSource, "scss/" + fileName + ".scss")
 
-const outputSource = path.join(currentWorkingPath, output);
-const outputStyles = path.join(currentWorkingPath, output + '/css');
+const outputSource = path.join(currentWorkingPath, output)
+const outputStyles = path.join(currentWorkingPath, output + "/css")
 
 /**
  * WPMU DEV Banner
@@ -39,15 +44,15 @@ const outputStyles = path.join(currentWorkingPath, output + '/css');
  */
 
 const banner = [
-	'/*!',
-	' * WPMU DEV Shared UI',
-	`${fileName !== 'sui-icons' ? ' * Icons Pack (' + fileName + ')' : ''}`,
-	' * ',
-	' * Copyright 2023 Incsub (https://incsub.com)',
-	' * Licensed under GPL-2.0 (http://www.gnu.org/licenses/gpl-2.0.html)',
-	' */',
-	'',
-].join('\n');
+	"/*!",
+	" * WPMU DEV Shared UI",
+	`${fileName !== "sui-icons" ? " * Icons Pack (" + fileName + ")" : ""}`,
+	" * ",
+	" * Copyright 2023 Incsub (https://incsub.com)",
+	" * Licensed under GPL-2.0 (http://www.gnu.org/licenses/gpl-2.0.html)",
+	" */",
+	"",
+].join("\n")
 
 /**
  * List of Supported Browsers
@@ -55,7 +60,7 @@ const banner = [
  * @since 1.0.0
  */
 
-const browsersList = ['last 2 version', '> 1%'];
+const browsersList = ["last 2 version", "> 1%"]
 
 /**
  * Building functions.
@@ -67,26 +72,43 @@ const browsersList = ['last 2 version', '> 1%'];
 function compile() {
 	return gulp
 		.src(inputStyles)
-		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+		.pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
 		.pipe(autoprefixer(browsersList))
 		.pipe(header(banner))
 		.pipe(prettier())
 		.pipe(gulp.dest(outputStyles))
 		.pipe(cleanCSS())
-		.pipe(rename({ suffix: '.min' }))
+		.pipe(rename({ suffix: ".min" }))
 		.pipe(gulp.dest(outputStyles))
-		.on('finish', function () {
-			console.log('📦 ' + packageName + ' finished compiling.');
-		});
+		.on("finish", function () {
+			console.log("📦 " + packageName + " finished compiling.")
+		})
 }
 
-function copy() {
+function copyFonts() {
 	return gulp
-		.src(inputSource + '/**')
-		.pipe(gulp.dest(outputSource))
-		.on('finish', function () {
-			console.log('📦 ' + packageName + ' finished copying files.');
-		});
+		.src(inputSource + "/fonts/**")
+		.pipe(gulp.dest(outputSource + "/fonts"))
+		.on("finish", function () {
+			console.log("📦 " + packageName + " finished copying files.")
+		})
+}
+
+function copySVG() {
+	return gulp
+		.src(inputSource + "/svg/**")
+		.pipe(gulp.dest(outputSource + "/svg"))
+		.on("finish", function () {
+			console.log("📦 " + packageName + " finished copying files.")
+		})
+}
+
+function tsBuild(cb) {
+	exec("npx tsup", (err, stdout, stderr) => {
+		console.log(stdout)
+		console.error(stderr)
+		cb(err)
+	})
 }
 
 /**
@@ -96,4 +118,4 @@ function copy() {
  * @since 1.0.0
  */
 
-exports.default = gulp.series(compile, copy);
+exports.default = gulp.series(tsBuild, compile, copyFonts, copySVG)
