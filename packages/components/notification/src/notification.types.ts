7266cb40de54ef15d52c9a9afa6bd13b6ffde1d7
douara <abderrahmandouara@gmail.com>
2023-10-19 19:24:29 +0800

    Fix(Notfication): Icons Type

--- packages/components/notification/src/notification.types.ts
+++ packages/components/notification/src/notification.types.ts
@@ -3,13 +3,15 @@
  */
 import { ReactNode } from "react"
 
+import { IconsNamesType } from "@wpmudev/sui-icons"
+
 // interface definition for the NotificationProps
 interface NotificationProps {
 	id: string // unique ID for the notification
 	title?: ReactNode // title content of the notification (can be any valid React node)
 	message?: ReactNode // message content of the notification (can be any valid React node)
 	action?: ReactNode // notification action
-	icon?: ReactNode | string // icon to display with the notification (can be a React node or a string)
+	icon?: IconsNamesType // icon to display with the notification (can be a React node or a string)
 	iconState?: string | never // icon state (unused in the current implementation)
 	isInline?: boolean // flag to indicate if the notification is displayed inline
 	isDismissible?: boolean // make notification dismissible
