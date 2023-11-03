4ca0f80b1c1782704f456a56b82a2448516b015c
douara <abderrahmandouara@gmail.com>
2023-11-03 23:46:15 +0700

    New(uploader): a11y tests

--- packages/ui/uploader/__tests__/uploader.test.tsx
+++ packages/ui/uploader/__tests__/uploader.test.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "@testing-library/jest-dom"
 import { screen, render } from "@testing-library/react"
-
+import { a11yTest } from "@wpmudev/sui-utils"
 import { Uploader, UploaderProps } from "../src"
 
 describe("@wpmudev/sui-uploader", () => {
@@ -15,5 +15,10 @@ describe("@wpmudev/sui-uploader", () => {
 		render(<Component />)
 		// Assert that the uploader element is in the document
 		expect(screen.getByTestId("uploader")).toBeInTheDocument()
+	})
+
+	// eslint-disable-next-line jest/expect-expect
+	it("passes a11y test", async () => {
+		await a11yTest(<Component />)
 	})
 })
