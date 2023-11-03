ceda1f178fa1abe8fa67f15e8aabae0676e726ae
douara <abderrahmandouara@gmail.com>
2023-11-03 21:23:25 +0700

    New(Search): a11y tests

--- packages/ui/score/__tests__/score.test.tsx
+++ packages/ui/score/__tests__/score.test.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
 import { a11yTest } from "@wpmudev/sui-utils"
-
 import { Score } from "../src"
 
 describe("@wpmudev/sui-score", () => {
