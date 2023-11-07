6fda7512ba9e561681ae4de90dc30fe99426e035
douara <abderrahmandouara@gmail.com>
2023-11-07 17:18:25 +0700

    Fix warnings + added a11y tests for rich-text-editor component

--- packages/ui/rich-text-editor/__tests__/rich-text-editor.test.tsx
+++ packages/ui/rich-text-editor/__tests__/rich-text-editor.test.tsx
@@ -2,11 +2,17 @@ import React from "react"
 import { RichTextEditor } from "../src"
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
+import { a11yTest } from "@wpmudev/sui-utils"
 
 describe("@wpmudev/sui-rich-text-editor", () => {
 	// Test: Renders the Accordion component
 	it("renders the Rich Text Editor Component", () => {
 		render(<RichTextEditor />)
 		expect(screen.getByTestId("rich-text-editor")).toBeVisible()
+	})
+
+	// eslint-disable-next-line jest/expect-expect
+	it("passes a11y test", async () => {
+		await a11yTest(<RichTextEditor />)
 	})
 })
