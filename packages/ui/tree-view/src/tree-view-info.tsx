9dd61e8a432b5ea610484a30b37cdaaaf192462d
douara <abderrahmandouara@gmail.com>
2023-11-21 17:42:16 +0700

    Fix Accessibility: Checkbox Keyboard Navigation

--- packages/ui/tree-view/src/tree-view-info.tsx
+++ packages/ui/tree-view/src/tree-view-info.tsx
@@ -1,10 +1,15 @@
-import React, { useCallback, useContext, useState } from "react"
+import React, {
+	useCallback,
+	useContext,
+	useState,
+	useRef,
+	useEffect,
+} from "react"
 import { useInteraction } from "@wpmudev/sui-hooks"
-
-import { generateCN, isEmpty, handleOnKeyDown } from "@wpmudev/sui-utils"
+import { createPortal } from "react-dom"
+import { generateCN, handleOnKeyDown } from "@wpmudev/sui-utils"
 import { Checkbox } from "@wpmudev/sui-checkbox"
 import Icons from "@wpmudev/sui-icons"
-
 import { TreeViewContextProps, TreeViewInfoProps } from "./tree-view.types"
 import { TreeViewContext } from "./tree-view-context"
 
@@ -29,6 +34,20 @@ const TreeViewInfo: React.FC<TreeViewInfoProps> = ({
 	// Manage interaction methods
 	const [isHovered, isFocused, interactionMethods] = useInteraction({})
 	const [isChecked, setIsChecked] = useState<boolean>(false)
+
+	// The DOM  element where we render the Treeview Checkbox
+	const [checkboxDomContainer, setCheckBoxDomContainer] =
+		useState<Element | null>(null)
+
+	// The element ref where we render the checkbox
+	const checkboxPortalRef = useRef(null)
+
+	// Updating the DOM element state when its "ref" becomes available
+	useEffect(() => {
+		if (checkboxPortalRef.current) {
+			setCheckBoxDomContainer(checkboxPortalRef.current)
+		}
+	}, [checkboxPortalRef])
 
 	// Generate class names
 	const classNames = generateCN("sui-tree-view__info", {
@@ -69,32 +88,47 @@ const TreeViewInfo: React.FC<TreeViewInfoProps> = ({
 	}, [])
 
 	return (
-		<div
-			tabIndex={isDisabled ? -1 : 0}
-			role="button"
-			className={classNames}
-			onClick={onClick}
-			onKeyDown={(e) => handleOnKeyDown(e, onClick)}
-			id={id}
-			data-testid={isGroup ? "" : "tree-view-item-info"}
-			{...(interactionMethods ?? {})}
-		>
-			{isGroup && <TickIcon size="sm" className="sui-tree-view__info-icon" />}
-			{ctx?.allowCheck && (
-				<div className="sui-tree-view__info-check">
-					{/* Render the Checkbox component for item selection */}
-					<Checkbox onChange={onCheckClick} isChecked={isChecked} />
-				</div>
-			)}
-			<div className="sui-tree-view__info-title">
-				{!!ItemIcon && (!!ctx?.showIcons || isDisabled) && (
-					// Render the item's icon, if available and allowed
-					<ItemIcon size="sm" className="sui-tree-view__info-icon" />
+		<>
+			{/* 
+				Rendering the Checkbox component outside the Tree View Info, to comply with
+				the accessibility principle that "Interactive controls must not be nested." 
+				This approach ensures that the Checkbox can be interacted with by all users, 
+				including those relying on assistive technologies, without violating accessibility guidelines.
+			 */}
+			{ctx?.allowCheck &&
+				checkboxDomContainer &&
+				createPortal(
+					<Checkbox onChange={onCheckClick} isChecked={isChecked} />,
+					checkboxDomContainer,
 				)}
-				{/* Render the item's title */}
-				<span id={`${id}-title`}>{children}</span>
+			<div
+				tabIndex={isDisabled ? -1 : 0}
+				role="button"
+				className={classNames}
+				onClick={onClick}
+				onKeyDown={(e) => handleOnKeyDown(e, onClick)}
+				id={id}
+				data-testid={isGroup ? "" : "tree-view-item-info"}
+				{...(interactionMethods ?? {})}
+			>
+				{isGroup && <TickIcon size="sm" className="sui-tree-view__info-icon" />}
+				{ctx?.allowCheck && (
+					<div className="sui-tree-view__info-check">
+						{/* Render the Checkbox component for item selection */}
+						{/* The container where the checkbox element will be displayed if the Treeview has a Checkbox */}
+						<div ref={checkboxPortalRef} />
+					</div>
+				)}
+				<div className="sui-tree-view__info-title">
+					{!!ItemIcon && (!!ctx?.showIcons || isDisabled) && (
+						// Render the item's icon, if available and allowed
+						<ItemIcon size="sm" className="sui-tree-view__info-icon" />
+					)}
+					{/* Render the item's title */}
+					<span id={`${id}-title`}>{children}</span>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
