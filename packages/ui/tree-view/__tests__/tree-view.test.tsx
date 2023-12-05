80102404e5b8c3f482cd72759fa7832d15596dac
douara <abderrahmandouara@gmail.com>
2023-12-05 17:02:52 +0800

    update(TreeView): unit tests for checkboxes

--- packages/ui/tree-view/__tests__/tree-view.test.tsx
+++ packages/ui/tree-view/__tests__/tree-view.test.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "@testing-library/jest-dom"
-import { render, screen, prettyDOM } from "@testing-library/react"
+import { render, screen, prettyDOM, fireEvent } from "@testing-library/react"
 import { a11yTest } from "@wpmudev/sui-utils"
 import { TreeView, TreeViewGroup, TreeViewItem, TreeViewProps } from "../src"
 
@@ -126,5 +126,62 @@ describe("@wpmudev/sui-tree-view", () => {
 				},
 			},
 		)
+	})
+
+	// Checkboxes tests
+	it("Checboxes work as expected", () => {
+		const { container } = render(
+			<TreeView allowCheck={true}>
+				<TreeViewGroup id="group-1" title="Tree Group">
+					<TreeViewGroup id="group-2" title="Tree Group">
+						<TreeViewItem id="item-1" isDisabled={true}>
+							Tree Item
+						</TreeViewItem>
+						<TreeViewItem id="item-2" isDisabled={true}>
+							Tree Item
+						</TreeViewItem>
+						<TreeViewGroup id="group-3" title="Tree Group">
+							<TreeViewItem id="item-4">Tree Item</TreeViewItem>
+							<TreeViewItem id="item-5">Tree Item</TreeViewItem>
+							<TreeViewItem id="item-6">Tree Item</TreeViewItem>
+						</TreeViewGroup>
+					</TreeViewGroup>
+				</TreeViewGroup>
+			</TreeView>,
+		)
+
+		/*
+		 * Clicking on an item checkbox
+		 */
+		const ItemFourCheckbox = container
+			.querySelector("#item-4")
+			?.querySelector(".sui-checkbox")
+
+		fireEvent.click(ItemFourCheckbox as Element)
+
+		// expect the checkbox of item-4 to be checked
+		expect(ItemFourCheckbox).toHaveClass("sui-checkbox--checked")
+
+		// Expect group-3 & group-1 state to be indeterminate
+		const GroupThreeCheckbox = container
+			.querySelector("#group-3")
+			?.querySelector(".sui-checkbox")
+		const GroupOneCheckbox = container
+			.querySelector("#group-1")
+			?.querySelector(".sui-checkbox")
+
+		expect(GroupThreeCheckbox).toHaveClass("sui-checkbox--indeterminate")
+		expect(GroupOneCheckbox).toHaveClass("sui-checkbox--indeterminate")
+
+		/*
+		 * Clicking on group parent will select all items
+		 */
+		fireEvent.click(GroupOneCheckbox as Element)
+
+		const allCheckboxes = container.querySelectorAll(".sui-checkbox")
+
+		allCheckboxes.forEach((checkbox) => {
+			expect(checkbox).toHaveClass("sui-checkbox--checked")
+		})
 	})
 })
