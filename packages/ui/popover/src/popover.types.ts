68c19257028c63f3a1a9b47c4b2ae3b8adf10732
Govind Kumar <gkprmr@gmail.com>
2023-12-29 12:15:51 +0530

    fix(popover): exclude title from props list types

--- packages/ui/popover/src/popover.types.ts
+++ packages/ui/popover/src/popover.types.ts
@@ -8,7 +8,8 @@ import { ButtonProps } from "@wpmudev/sui-button"
 interface PopoverBaseProps
 	extends Omit<
 		HTMLProps<HTMLButtonElement | HTMLSpanElement | HTMLAnchorElement>,
-		"onClick"
+		"onClick",
+		"title"
 	> {
 	/**
 	 * Open popup on initial load
