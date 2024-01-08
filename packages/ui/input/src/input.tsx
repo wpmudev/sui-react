9861fd7d83a5ffe8ef347b005a4ef2356f4d7efa
Pawan Kumar <pwnkumar201@gmail.com>
2024-01-08 13:46:33 +0530

    ✨ new(icon): updated input icon to svg.

--- packages/ui/input/src/input.tsx
+++ packages/ui/input/src/input.tsx
@@ -170,8 +170,8 @@ const Input: ForwardRefExoticComponent<PropsWithoutRef<InputProps>> =
 				<div className={classNames} data-testid="input">
 					{icon && !isMultiLine && "start" === iconPosition && (
 						<Icon
-							name={icon ?? ""}
-							size={isSmall ? "md" : "lg"}
+							name={icon}
+							size={isSmall ? "sm" : "md"}
 							position={iconPosition}
 						/>
 					)}
@@ -194,8 +194,8 @@ const Input: ForwardRefExoticComponent<PropsWithoutRef<InputProps>> =
 					</div>
 					{icon && !isMultiLine && "end" === iconPosition && (
 						<Icon
-							name={icon ?? ""}
-							size={isSmall ? "md" : "lg"}
+							name={icon}
+							size={isSmall ? "sm" : "md"}
 							position={iconPosition}
 						/>
 					)}
