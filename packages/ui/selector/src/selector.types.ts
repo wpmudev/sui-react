c8821bad4ec03736d6a3fcc95f66a135017a856c
Govind Kumar <gkprmr@gmail.com>
2024-01-08 11:35:12 +0530

    fix(selector): access type property correctly

--- packages/ui/selector/src/selector.types.ts
+++ packages/ui/selector/src/selector.types.ts
@@ -51,22 +51,18 @@ interface SelectorProps
 		| "navy"
 }
 
-interface SelectorOptionProps
-	extends Pick<
-		SelectorProps,
-		| "iconOrBrandUrl"
-		| "title"
-		| "description"
-		| "imageUrl"
-		| "isPro"
-		| "isChecked"
-		| "alignment"
-		| "variation"
-		| "tag"
-		| "allowRemove"
-		| "onRemove"
-		| "tagColor"
-		| "isPro"
-	> {}
+interface SelectorOptionProps {
+	iconOrBrandUrl: SelectorProps["iconOrBrandUrl"]
+	title: SelectorProps["title"]
+	description: SelectorProps["description"]
+	imageUrl: SelectorProps["imageUrl"]
+	isChecked: SelectorProps["isChecked"]
+	alignment: SelectorProps["alignment"]
+	variation: SelectorProps["variation"]
+	tag: SelectorProps["tag"]
+	onRemove: SelectorProps["onRemove"]
+	tagColor: SelectorProps["tagColor"]
+	isPro: SelectorProps["isPro"]
+}
 
 export type { SelectorProps, SelectorOptionProps }
