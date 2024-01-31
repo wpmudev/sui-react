f3259fea2a27b4822fb9116ac605333342cad859
Govind Kumar <gkprmr@gmail.com>
2024-01-31 17:05:10 +0530

    fix(use-default-children): code improvements

--- packages/hooks/src/use-default-children.ts
+++ packages/hooks/src/use-default-children.ts
@@ -2,9 +2,11 @@
  * The useDefaultChildren hook is defined as a functional component,
  * its role is to display a customized content when children prop is empty
  */
+import { ReactNode } from "react"
+
 const useDefaultChildren = (
-	children: React.ReactNode, // the component children prop
-	content?: React.ReactNode, // the default content to display
+	children: ReactNode, // the component children prop
+	content?: ReactNode, // the default content to display
 ) => {
 	return children || content || "{children content}"
 }
