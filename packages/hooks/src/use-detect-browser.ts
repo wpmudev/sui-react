cc5c90c4bc00cd8cd62ad94c055ff49644d0a965
Govind Kumar <gkprmr@gmail.com>
2024-01-31 17:13:28 +0530

    fix(use-detect-browser): test failed

--- packages/hooks/src/use-detect-browser.ts
+++ packages/hooks/src/use-detect-browser.ts
@@ -4,29 +4,29 @@
 const useDetectBrowser = () => {
 	const userAgent: string = navigator?.userAgent ?? ""
 
-	let name = "unknown"
-	let ver = ""
+	let name: string | undefined = "unknown"
+	let ver: string | undefined = ""
 
 	switch (true) {
 		case userAgent.indexOf("Firefox") > -1:
 			name = "firefox"
-			ver = userAgent?.match(/Firefox\/([0-9.]+)/)[1]
+			ver = userAgent?.match(/Firefox\/([0-9.]+)/)?.[1]
 			break
 		case userAgent.indexOf("Chrome") > -1:
 			name = "chrome"
-			ver = userAgent?.match(/Chrome\/([0-9.]+)/)[1]
+			ver = userAgent?.match(/Chrome\/([0-9.]+)/)?.[1]
 			break
 		case userAgent.indexOf("Safari") > -1:
 			name = "safari"
-			ver = userAgent?.match(/Version\/([0-9.]+)/)[1]
+			ver = userAgent?.match(/Version\/([0-9.]+)/)?.[1]
 			break
 		case userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident/") > -1:
 			name = "ie"
-			ver = userAgent?.match(/(?:MSIE |rv:)([0-9.]+)/)[1]
+			ver = userAgent?.match(/(?:MSIE |rv:)([0-9.]+)/)?.[1]
 			break
 		case userAgent.indexOf("Edge") > -1:
 			name = "ms-edge"
-			ver = userAgent?.match(/Edge\/([0-9.]+)/)[1]
+			ver = userAgent?.match(/Edge\/([0-9.]+)/)?.[1]
 			break
 	}
 
