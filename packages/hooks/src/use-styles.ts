42e5b9b3e238e0ed3fe67f6f24eead2f05d341a0
Govind Kumar <gkprmr@gmail.com>
2024-02-09 06:24:26 +0530

    fix(hooks): improve useStyles hook

--- packages/hooks/src/use-styles.ts
+++ packages/hooks/src/use-styles.ts
@@ -1,15 +1,6 @@
-import jss, { createGenerateId } from "jss"
-import preset from "jss-preset-default"
+import { createUseStyles } from "react-jss"
 import { CSSProperties } from "react"
 import { generateCN } from "@wpmudev/sui-utils"
-
-// one time setup with default plugins and settings.
-jss.setup(preset())
-
-// create a custom generateId function that generates random class names
-const generateId = createGenerateId({
-	minify: true,
-})
 
 // custom classname prefix
 const SUI_PREFIX = "sui-custom"
@@ -83,31 +74,48 @@ export const buildStyleSheet = (
 	propName: string,
 	value: StringPropertyType,
 ): { [key: string]: string } => {
-	// Convert to valid CSS property using shorthand maps if available
-	propName = CSS_SHORTHAND_MAPS[propName] ?? propName
+	const shorthandPropName = CSS_SHORTHAND_MAPS[propName] ?? propName
+
+	// build single value
+	const buildSingleValue = (val: string) => ({ [shorthandPropName]: val })
+
+	// build media queries
+	const buildMediaQueries = (val: string, pos: number, acc: object) => {
+		// last breakpoint value is for default value
+		if (pos === 4) {
+			return buildSingleValue(val)
+		}
+
+		const prevSize = breakpoints[pos - 1]
+		const size = breakpoints[pos]
+		const styleVal = buildSingleValue(val)
+
+		const query =
+			pos > 0
+				? `@media(min-width:${prevSize + 1}px) and (max-width:${size}px)`
+				: `@media(max-width:${size}px)`
+
+		return { ...acc, [query]: styleVal }
+	}
 
 	switch (typeof value) {
 		case "string":
-			// for single string values, return a simple CSS property object
-			return { [propName]: value }
-
+			return buildSingleValue(value)
 		case "object":
-			// for object values, create responsive CSS
-			return value.reduce((acc, val, index) => {
-				val = { [propName]: val }
-				const size = breakpoints[index]
-				const query = index === 4 ? "" : `@media(max-width:${size}px)`
-
-				return {
+			return value.reduce(
+				(acc, val, index) => ({
 					...acc,
-					[query]: val,
-				}
-			}, {})
+					...buildMediaQueries(val, index, acc),
+				}),
+				{},
+			)
 
 		default:
 			return {}
 	}
 }
+
+type isValidCSSProperty<T, K extends string> = K extends keyof T ? true : false
 
 /**
  * SUI custom hook for generating className based on passed CSS properties
@@ -116,10 +124,18 @@ export const buildStyleSheet = (
  * @param {string}         attachWith Existing className
  */
 export const useStyles = (styleProps: useStylesTypes, attachWith = "") => {
-	const styles: Record<string, any> = { [`${SUI_PREFIX}`]: {} }
+	const styles: Record<string, any> = {}
 
 	// go through all props
 	for (const name of Object.keys(styleProps)) {
+		// check if valid CSS property
+		type isValid = isValidCSSProperty<CSSProperties, typeof name>
+
+		// skip if not a valid CSS property
+		if (false as isValid) {
+			continue
+		}
+
 		// build styles
 		styles[SUI_PREFIX] = {
 			...styles[SUI_PREFIX],
@@ -129,7 +145,10 @@ export const useStyles = (styleProps: useStylesTypes, attachWith = "") => {
 		}
 	}
 
-	// create classnames
-	const { classes } = jss?.createStyleSheet(styles, { generateId })?.attach()
-	return { cssCN: generateCN(attachWith, {}, classes?.[SUI_PREFIX]) }
+	// generated classnames
+	const generatedCN = createUseStyles(styles)
+
+	return {
+		cssCN: generateCN(attachWith, {}, generatedCN()?.[SUI_PREFIX]),
+	}
 }
