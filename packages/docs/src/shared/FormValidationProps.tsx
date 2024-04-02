3690f01daebaa6449d24b5edfdfcca1702a6f176
douara <abderrahmandouara@gmail.com>
2024-04-02 13:27:38 +0800

    update: form validation common component doc

--- packages/docs/src/shared/FormValidationProps.tsx
+++ packages/docs/src/shared/FormValidationProps.tsx
@@ -8,11 +8,12 @@ const FormValidationProps = () => {
 			contained={true}
 			style={{ paddingTop: 20, paddingBottom: 30, paddingLeft: 0 }}
 		>
-			For validating the value of this form component, use the{" "}
+			Utilize the{" "}
 			<a href="/?path=/docs/sui-npm-packages-hooks--docs" target="_blank">
 				useValidation
 			</a>{" "}
-			hook from the <Code small={true}>@wpmudev/sui-hooks</Code> package.
+			hook from the <Code small={true}>@wpmudev/sui-hooks</Code> package to
+			validate this form component&apos;s value.
 		</Section>
 	)
 }
