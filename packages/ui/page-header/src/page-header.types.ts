8e34f191dd9c37840adc99534d3e436546050350
work <abderrahmandouara@gmail.com>
2024-07-20 01:15:46 +0800

    Added page header component

---
+++ packages/ui/page-header/src/page-header.types.ts
@@ -0,0 +1,26 @@
+import React, { HTMLProps } from "react"
+import { SuiHTMLAttributes, SuiStyleType } from "@wpmudev/sui-utils"
+
+/**
+ * PageHeaderProps is an interface that extends the HTMLDivElement interface
+ * while allowing additional properties specific to the page header component.
+ * It is used to define the set of properties that can be passed to the page header component.
+ */
+interface PageHeaderProps
+	extends SuiHTMLAttributes<HTMLProps<HTMLDivElement>>,
+		SuiStyleType {
+	/**
+	 * The page title
+	 */
+	title?: string
+	/**
+	 * Actions for the header to be displayed on the right hand side
+	 */
+	actions?: React.ReactNode[]
+	/**
+	 * Additional className
+	 */
+	className?: string
+}
+
+export type { PageHeaderProps }
---
+++ packages/ui/page-header/stories/page-header.stories.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+
+// Import required component(s)
+import { PageHeader as SuiPageHeader, PageHeaderProps } from "../src"
+import { Button } from "@wpmudev/sui-button"
+
+// Import documentation main page
+import docs from "./page-header.mdx"
+
+export default {
+	title: "SUI/Components/Core/Page Header",
+	component: SuiPageHeader,
+	parameters: {
+		layout: "fullscreen",
+		docs: {
+			page: docs,
+		},
+	},
+}
+
+// Build story
+export const PageHeader = ({ ...props }: PageHeaderProps) => {
+	let backgroundColor = "#fff"
+
+	// @ts-ignore
+	if ("secondary" === props?.theme) {
+		backgroundColor = "#0059FF"
+	}
+
+	const box = {
+		margin: "0 0 20px",
+		padding: "30px",
+		border: "1px solid #E6E6E6",
+		borderRadius: "4px",
+		background: backgroundColor,
+	}
+
+	const actions = [
+		<Button key={0} type="primary" colorScheme="blue">
+			Primary Action
+		</Button>,
+		<Button key={1} type="secondary" colorScheme="black">
+			Secondary Action
+		</Button>,
+	]
+
+	return (
+		<div className="sui-layout sui-layout--horizontal sui-layout--vertical">
+			<div className="sui-layout__content">
+				<div style={box}>
+					<SuiPageHeader {...props} actions={actions} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+PageHeader.args = {
+	title: "Page Title",
+}
+
+PageHeader.argTypes = {}
