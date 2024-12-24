4c1bf535bee0f2878183d5d638381f8fb69146ae
douara <abderrahmandouara@gmail.com>
2024-12-24 14:43:09 +0800

    Added name prop for PasswordField

--- packages/ui/password-field/src/password-field.tsx
+++ packages/ui/password-field/src/password-field.tsx
@@ -18,6 +18,7 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
 	validate,
 	validateOnMount,
 	resetValidation,
+	name,
 	_htmlProps,
 	_style = {},
 }) => {
@@ -45,6 +46,7 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
 		validate,
 		validateOnMount,
 		resetValidation,
+		name,
 		onKeyUp: () => {
 			const inputElement = document.getElementById(
 				id as string,
