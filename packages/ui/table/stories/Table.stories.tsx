cd80b93b1542ac2ef06203f53767040c7ce0dc1d
Govind <gkprmr@gmail.com>
2026-07-06 04:38:03 +0530

    docs(table): remove un-used variable

--- packages/ui/table/stories/Table.stories.tsx
+++ packages/ui/table/stories/Table.stories.tsx
@@ -18,7 +18,6 @@ import { chunkArray } from "@wpmudev/sui-utils"
 
 // Import documentation main page
 import docs from "./ReactTable.mdx"
-import { Box, BoxGroup } from "@wpmudev/sui-box"
 
 // Configure default options
 export default {
